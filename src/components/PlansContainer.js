cbda22d47e5f0f810b093d3a3f11bf470ae1cde1
clair13 <theclairvoyant1313@yahoo.gr>
2017-08-28 21:04:07 +0300

    Install axios and create PlansContainer

--- src/components/PlansContainer.js
+++ src/components/PlansContainer.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class PlansContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      plans: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001.api/v1/plans.json')
+    .then(response => {
+      console.log(response)
+      this.setState({plans: response.data})
+    })
+    .catch(error => console.log(error))
+  }
   render() {
     return (
       <div className="">
