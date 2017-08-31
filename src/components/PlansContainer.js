95fdc80b912b9a7805dfb9803ec775aebff56a6d
clair13 <theclairvoyant1313@yahoo.gr>
2017-08-31 18:22:56 +0300

    Install immutability-helper

--- src/components/PlansContainer.js
+++ src/components/PlansContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import Plan from './Plan'
+import update from 'immutability-helper'
 
 class PlansContainer extends Component {
   constructor(props) {
@@ -21,10 +22,13 @@ class PlansContainer extends Component {
   addNewPlan = () => {
     axios.post('http://localhost:3001/api/v1/plans', {plan: {title: '', body: ''}})
       .then(response => {
-        console.log(response) })
+        console.log(response)
+        const plans =update(this.state.plans, {$splice: [[0, 0, response.data]]})
+        this.setState({plans: plans })
+      })
       .catch(error => console.log(error))
   }
-  
+
   render() {
     return (
       <div>
